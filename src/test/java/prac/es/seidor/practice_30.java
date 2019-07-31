be6719d7aaec4bb2bc92ba21af6464968d3e38b8
jay-record <hjk940510@gmail.com>
2019-07-31 14:15:28 +0200

    practice

--- src/test/java/prac/es/seidor/practice_30.java
+++ src/test/java/prac/es/seidor/practice_30.java
@@ -1,10 +1,53 @@
 package prac.es.seidor;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class practice_30 extends practice_base{
     @Test
     public void test30(){
+
+        //Call Browser
+        String BaseUrl = "http://www.automationPUB.com";
+        webDriver.navigate().to(BaseUrl);
+
+        //Access & click the  PRACTICE SITE
+        Actions action = new Actions(webDriver);
+        WebElement Menu = webDriver.findElement(By.xpath("//a[@href='https://automationpub.com/category/uncategorized/']"));
+        action.moveToElement(Menu).perform();
+        webDriver.findElement(By.id("menu-item-42")).click();
+
+        //CLlick the New User
+        List<WebElement> tablinks1 = webDriver.findElements(By.className("tablinks"));
+        tablinks1.get(1).click();
+
+        //Put the email & pw
+        WebElement name = webDriver.findElement(By.id("name"));
+        name.sendKeys("HONGJAE");
+        WebElement email2 =webDriver.findElement(By.id("email2"));
+        email2.sendKeys("TestID4550@gmail.com");
+        WebElement password2 = webDriver.findElement(By.id("password2"));
+        password2.sendKeys("Test4550");
+        name.submit();
+
+        //Log-In
+        List<WebElement> tablinks2 = webDriver.findElements(By.className("tablinks"));
+        tablinks2.get(0).click();
+        WebElement email1 =webDriver.findElement(By.id("email1"));
+        email1.sendKeys("TestID4550@gmail.com");
+        WebElement password1 = webDriver.findElement(By.id("password1"));
+        password1.sendKeys("Test4550");
+        email1.submit();
+        pause(1);
+
+        //logout
+        webDriver.findElement(By.className("caret")).click();
+        webDriver.findElement(By.name("logout")).click();
+        pause(3);
 
     }
 }
